da8846b8ffb8a3b860660230f6345495785ff1e2
nmht <nmhoangthien@gmail.com>
2022-07-04 01:02:35 +0700

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+/* eslint-disable */
+
 module.exports = {
 	content: ["./*.html"],
 	theme: {
 		extend: {
-			screens: {
-				sm: "480px",
+			spacing: {
+				big: "48rem",
 			},
+		},
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
 		},
 		fontFamily: {
 			nunito: ["Nunito"],
@@ -13,3 +22,5 @@ module.exports = {
 	},
 	plugins: [],
 };
+
+/* eslint-enable */
